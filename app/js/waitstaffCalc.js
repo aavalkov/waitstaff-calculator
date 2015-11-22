2f9e0b7b7b8b70b056c4f03a9898a9f1f4221355
Anna Valkov <anna.a.valkov@gmail.com>
2015-11-22 11:58:44 -0800

    separte the front end code and add routing for each section

--- app/js/waitstaffCalc.js
+++ app/js/waitstaffCalc.js
@@ -1,46 +1,66 @@
-angular.module('waitstaffCalc', [])
+angular.module('waitstaffCalc', ['ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider.when('/',{
+			templateUrl:'home.html',
+			controller:'HomeCtrl'
+		}).when('/new-meal', {
+			templateUrl:'newMeal.html',
+			controller: 'NewMealCtrl'
+		}).when('/my-earnings',{
+			templateUrl:'myEarnings.html',
+			controller: 'EarningsCtrl'
+		})
+	}).controller('HomeCtrl', function($rootScope){
+		//empty
+	})
+	.controller('NewMealCtrl', function($rootScope){
+		//empty
+	})
+	.controller('EarningsCtrl', function($rootScope){
+
+	})
 	.controller('waitstaffCalcController', function($scope){
-		$scope.tipTotal = 0;
-		$scope.mealCount = 0;
+		// $scope.tipTotal = 0;
+		// $scope.mealCount = 0;
 
-		$scope.submit = function(){
-			if ($scope.mealDetails.$valid){
-				console.log('The form is valid');
-				customerCharges();
-			} else {
-				console.log('The form is invalid');
-			}
-		};
+		// $scope.submit = function(){
+		// 	if ($scope.mealDetails.$valid){
+		// 		console.log('The form is valid');
+		// 		customerCharges();
+		// 	} else {
+		// 		console.log('The form is invalid');
+		// 	}
+		// };
 
-		$scope.cancel = function(){
-			$scope.data = [];
-		};
+		// $scope.cancel = function(){
+		// 	$scope.data = [];
+		// };
 
-		customerCharges = function(){
-			var price = parseInt($scope.data.price);
-			var tax = parseInt($scope.data.tax);
-			var tip = parseInt($scope.data.tip);
+		// customerCharges = function(){
+		// 	var price = parseInt($scope.data.price);
+		// 	var tax = parseInt($scope.data.tax);
+		// 	var tip = parseInt($scope.data.tip);
 
-			$scope.subtotal = (tax/100 * price) + price;
-			$scope.chargeTip = tip/100 * price;
-			earningsInfo($scope.chargeTip);
-		};
+		// 	$scope.subtotal = (tax/100 * price) + price;
+		// 	$scope.chargeTip = tip/100 * price;
+		// 	earningsInfo($scope.chargeTip);
+		// };
 
-		earningsInfo = function(tipAmount){
-			$scope.tipTotal += parseInt(tipAmount);
-			$scope.mealCount += 1;
-			$scope.avgTip = $scope.tipTotal/$scope.mealCount;
-		};
+		// earningsInfo = function(tipAmount){
+		// 	$scope.tipTotal += parseInt(tipAmount);
+		// 	$scope.mealCount += 1;
+		// 	$scope.avgTip = $scope.tipTotal/$scope.mealCount;
+		// };
 
-		$scope.reset = function(){
-			$scope.data = [];
-			$scope.mealDetails.$setPristine();
-			$scope.subtotal = "";
-			$scope.chargeTip = "";
-			$scope.tipTotal = "";
-			$scope.mealCount = "";
-			$scope.avgTip = "";
-		}
+		// $scope.reset = function(){
+		// 	$scope.data = [];
+		// 	$scope.mealDetails.$setPristine();
+		// 	$scope.subtotal = "";
+		// 	$scope.chargeTip = "";
+		// 	$scope.tipTotal = "";
+		// 	$scope.mealCount = "";
+		// 	$scope.avgTip = "";
+		// }
 	});
 
 
